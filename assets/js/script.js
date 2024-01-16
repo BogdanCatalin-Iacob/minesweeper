c3f4c9d6769d6eaffb7ef63abfb29d17027eceb5
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-01-16 19:16:23 +0000

    Refactor: for loop create cell element and add event listener to it

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,15 @@ const scoreCounter = document.getElementsByClassName('score-counter')[0];
 const endGameScreen = document.getElementsByClassName('end-game-screen')[0];
 const endGameText = document.getElementsByClassName('end-game-text')[0];
 
-for (let i = 0; i < 100; i++){
-    grid.innerHTML += `<div class="cell">${i + 1}</div>`;
+const totalCells = 100;
+
+for (let i = 0; i < totalCells; i++){
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+
+    cell.addEventListener('click', function() {
+        console.log(`clicked: ${i}`);
+    });
+
+    grid.appendChild(cell);
 }
