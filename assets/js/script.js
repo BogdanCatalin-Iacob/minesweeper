e359488377f5f7874cce4891844bd8fa19c78d5a
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-01-16 19:02:55 +0000

    Get elements into js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-let grid = document.getElementsByClassName('grid')[0];
+const grid = document.getElementsByClassName('grid')[0];
+const scoreCounter = document.getElementsByClassName('score-counter')[0];
+const endGameScreen = document.getElementsByClassName('end-game-screen')[0];
+const endGameText = document.getElementsByClassName('end-game-text')[0];
 
 for (let i = 0; i < 100; i++){
     grid.innerHTML += `<div class="cell">${i + 1}</div>`;
